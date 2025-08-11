497f5e7f0551f8f4a48d9fceeaae6b862a1b9e86
mohammad2745 <mohammadfcc2745@gmail.com>
2025-08-11 19:47:05 +0600

    Task router and model created

---
+++ src/models/task.model.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Task = sequelize.define("Task", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  isCompleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Task;
